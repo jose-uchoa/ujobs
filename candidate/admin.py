ec3718dbacaa5b280e6f152f50f42f7c13f57a11
“jose-uchoa” <jooseuchoa@gmail.com>
2023-11-03 21:25:15 -0300

    raw: create models

--- candidate/admin.py
+++ candidate/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CandidateProfile, CandidateRequests
+
+admin.site.register(CandidateProfile)
+admin.site.register(CandidateRequests)
--- candidate/models.py
+++ candidate/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from company.models import Job
+from core.models import User
+
+
+class CandidateProfile(models.Model):
+    candidate = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="profile"
+    )
+    resume = models.FileField(null=True, upload_to="user_resume")
+
+    def __str__(self):
+        return f"Perfil de {self.candidate}"
+
+
+class CandidateRequests(models.Model):
+    requests = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="requests"
+    )
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    status = models.CharField(max_length=80, default="Em análise")
+
+    def __str__(self):
+        return f"Solicitação de {self.requests} para {self.job}"
+
+    def save(self, *args, **kwargs):
+        super(CandidateRequests, self).save(*args, **kwargs)
